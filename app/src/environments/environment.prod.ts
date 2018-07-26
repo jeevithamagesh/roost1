d470835b5d6f25006d6afee2d6470486e96dcbbd
Jeevitha <you@example.com>
2018-07-26 12:38:42 +0530

    Initial commit

---
+++ app/src/environments/environment.prod.ts
@@ -0,0 +1,23 @@
+export const environment = {
+    "envId": "767d006c-6aac-44bf-a3b2-98537a4dde40",
+    "name": "prod",
+    "properties": {
+        "production": true,
+        "baseUrl": "http://localhost:3000/bhive-art/",
+        "tenantName": "neutrinos-testing",
+        "appName": "roostapp",
+        "namespace": "com.neutrinos.neutrinos-testing.roostapp",
+        "isNotificationEnabled": false,
+        "googleMapKey": "AIzaSyCSTnVwijjv0CFRA4MEeS-H6PAQc87LEoU",
+        "firebaseSenderId": "FIREBASE_SENDER_ID",
+        "firebaseAuthKey": "FIREBASE_AUTH_KEY",
+        "authDomain": "FIREBASE_AUTH_DOMAIN",
+        "databaseURL": "FIREBASE_DATABASE_URL",
+        "storageBucket": "FIREBASE_STORAGE_BUCKET",
+        "messagingSenderId": "FIREBASE_SENDER_ID",
+        "appDataSource": "APP_DATA_SOURCE",
+        "appAuthenticationStrategy": "basicAuth",
+        "basicAuthUser": "username",
+        "basicAuthPassword": "password"
+    }
+}
---
+++ app/src/environments/environment.ts
@@ -0,0 +1,23 @@
+export const environment = {
+    "envId": "c55e6670-592a-97ad-0d95-57209663759f",
+    "name": "dev",
+    "properties": {
+        "production": false,
+        "baseUrl": "http://localhost:3000/bhive-art/",
+        "tenantName": "neutrinos-testing",
+        "appName": "roostapp",
+        "namespace": "com.neutrinos.neutrinos-testing.roostapp",
+        "isNotificationEnabled": false,
+        "googleMapKey": "AIzaSyCSTnVwijjv0CFRA4MEeS-H6PAQc87LEoU",
+        "firebaseSenderId": "FIREBASE_SENDER_ID",
+        "firebaseAuthKey": "FIREBASE_AUTH_KEY",
+        "authDomain": "FIREBASE_AUTH_DOMAIN",
+        "databaseURL": "FIREBASE_DATABASE_URL",
+        "storageBucket": "FIREBASE_STORAGE_BUCKET",
+        "messagingSenderId": "FIREBASE_SENDER_ID",
+        "appDataSource": "APP_DATA_SOURCE",
+        "appAuthenticationStrategy": "localAuth",
+        "basicAuthUser": "username",
+        "basicAuthPassword": "password"
+    }
+}
